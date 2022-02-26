da4e1a3889155348de395711170547ec2364a713
fanaimi <fanaimi@users.noreply.github.com>
2022-02-26 15:15:06 +0000

    troubles with addForce, will revert back to transform positioning

--- evade-enemy/Assets/Scripts/PlayerController.cs
+++ evade-enemy/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
+    [SerializeField] private float m_MoveSpeed;
+    [SerializeField] private float m_TurnSpeed;
+    private float m_DeadZone = .2f;
+
+    private Rigidbody m_Rb;
+    private Joystick m_Joystick;
+
+    private void Awake()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+        m_Joystick = FindObjectOfType<Joystick>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        ListenToJoystick();
+    }
+
+    private void ListenToJoystick()
+    {
+        if (m_Joystick.Direction.magnitude > m_DeadZone)
+        {
+            // MOVING
+   
+        }
+        else
+        {
+            // STOPPED
+        }
     }
 }
