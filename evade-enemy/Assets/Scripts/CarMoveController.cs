2156f1d7f69aef3c8b8bb35e4471d28ae70e8447
fanaimi <fanaimi@users.noreply.github.com>
2022-02-27 11:44:14 +0000

    working on CarMoveController

---
+++ evade-enemy/Assets/Scripts/CarMoveController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMoveController : MonoBehaviour
+{
+   [SerializeField] private WheelCollider m_FLwheel;
+   [SerializeField] private WheelCollider m_FRwheel;
+   [SerializeField] private WheelCollider m_BLwheel;
+   [SerializeField] private WheelCollider m_BRwheel;
+}
