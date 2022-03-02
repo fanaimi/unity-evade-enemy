75923c2bdd60aaea6410693a0b78993f711bb77f
fanaimi <fanaimi@users.noreply.github.com>
2022-03-02 12:50:38 +0000

    working on speedometer

---
+++ evade-enemy/Assets/Scripts/Speedometer.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speedometer : MonoBehaviour
+{
+
+    private const float MAX_SPEED_ANGLE = 220f;
+    private const float MIN_SPEED_ANGLE = -30f;
+    
+    private Transform m_SpeedNeedleTr;
+
+
+    private float m_CurrentSpeed;
+    private float m_MaxSpeed;
+    
+
+    private void Awake()
+    {
+        m_SpeedNeedleTr = transform.Find("SpeedNeedle");
+        m_CurrentSpeed = 0f;
+        m_MaxSpeed = 120f;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(m_SpeedNeedleTr.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    float GetSpeedNedleRotation()
+    {
+        float totalRotationAngle = MIN_SPEED_ANGLE - MAX_SPEED_ANGLE;
+
+        float normalizedSpeed = m_CurrentSpeed / m_MaxSpeed; // value between 0 -> 1
+
+        return MIN_SPEED_ANGLE - totalRotationAngle * normalizedSpeed;
+    }
+
+}
